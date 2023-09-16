2be9f14fd5a48a6f677ac748dea4e475970adc19
AWieclawski <a_wieclawski@poczta.pl>
2023-09-16 07:57:55 +0200

    Init

---
+++ src/main/java/edu/awieclawski/microservicescloudspring/MicroservicesCloudSpringApplication.java
@@ -0,0 +1,13 @@
+package edu.awieclawski.microservicescloudspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesCloudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesCloudSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/awieclawski/microservicescloudspring/MicroservicesCloudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.awieclawski.microservicescloudspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesCloudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
